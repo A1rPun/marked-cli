192a3590acb803b5716d7ee46d04402601621587
A1rPun <thombakker@gmail.com>
2019-05-07 01:37:03 +0200

    :rainbow: Better colors + cleanup

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # marked-cli
-Simple markdown to terminal
+
+> Simple markdown to terminal formatter
 
 ## TODO
 
-- Header ascii
-- Emoji
-- Tables
-4. Numbered lists
-- Generic syntax highlighting
+### Must have
+
+- [x] Colors!
+- [x] Blockquote
+- [x] Code block
+- [x] Unordered list & task list
+- [x] Links & images
+- [ ] Equally spaced tables
+- [ ] Numbered list
+- [ ] Sub items in list
+
+### Should Have
+
+- [ ] Color customization
+- [ ] Text customization (listitem, horizontal row)
+
+### Could have
+
+- [ ] Headers in ascii style
+- [ ] Emoji
+- [ ] Generic syntax highlighting in code block
--- terminal.js
+++ terminal.js
@@ -1,40 +1,59 @@
 const chalk = require("chalk");
 
 const newline = "\n";
+const space = " ";
+const horizontalRow = "─";
+const listItem = "•";
+const checkedBox = "\u2611";
+const emptyBox = "\u2610";
 const block = text => text + newline + newline;
-const width = process.stdout.columns - 4;
+const width = process.stdout.columns;
 
 module.exports = {
   // Block elements
   blockquote: quote => {
-    const prepend = chalk.bgWhite(" ") + " ";
+    const prepend = `${chalk.bgYellow(space)} `;
     let currentWidth = 0;
-    const block = quote.match(/\S+/g).reduce((acc, cur) => {
-      currentWidth += cur.length;
-      if (currentWidth > width) {
-        acc.push([cur]);
-	currentWidth = 0;
-      } else {
-	acc[acc.length - 1].push(cur);
-      }
-      currentWidth += cur.length;
-      return acc;
-    }, [[]]).map(x => x.join(" "))
+    const fixedQuote = quote
+      .match(/\S+/g)
+      .reduce(
+        (acc, cur) => {
+          currentWidth += cur.length + space.length;
+          if (currentWidth > width - 2) { // 2 = prepend length
+            acc.push([cur]);
+            currentWidth = cur.length + space.length;
+          } else {
+            acc[acc.length - 1].push(cur);
+          }
+          return acc;
+        },
+        [[]]
+      )
+      .map(x => x.join(space))
       .join(newline + prepend);
-    return prepend + chalk.yellow(block);
+    return block(prepend + chalk.yellow(fixedQuote));
   },
-  checkbox: checked => (checked ? "\u2611 " : "\u2610 "),
+  checkbox: checked => chalk.red(`${checked ? checkedBox : emptyBox} `),
   code: (code, infostring, escaped) => {
-    let codes = code.split("\n");
-    let max = Math.max(...codes.map(x => x.length));
-    codes = codes.map(x => x.padEnd(max)).join("\n");
-    return block(chalk.inverse(codes));
+    let codes = code
+      .split(newline)
+      .map(x => chalk.inverse(x.padEnd(width)))
+      .join(newline);
+    return block(codes);
   },
-  heading: (text, level, raw, slugger) => block(chalk.red.bold.underline(`${"#".repeat(level)} ${text}`)),
-  hr: () => block(" ─── "),
+  heading: (text, level, raw, slugger) => {
+    let format = chalk.red;
+    if (level < 3) format = format.bold;
+    if (level < 5) format = format.underline;
+    return block(format(text.toUpperCase()));
+  },
+  hr: () => block(chalk.magenta(horizontalRow.repeat(width))),
   html: html => html,
   list: (body, ordered, start) => body + newline,
-  listitem: (text, task) => ` ${task ? "" : "•"} ${chalk.white(text)}${newline}`,
+  listitem: (text, task) => {
+    const prefix = task ? "" : chalk.red(listItem);
+    return ` ${prefix} ${chalk.white(text)}${newline}`;
+  },
   paragraph: text => block(text),
   table: (header, body) => block(chalk.bold(header) + body),
   tablecell: (content, flags) => content + "\t│",
@@ -43,9 +62,9 @@ module.exports = {
   br: () => newline,
   codespan: code => chalk.inverse(code),
   del: text => chalk.strikethrough(text),
-  em: text => chalk.italic(text),
-  image: function (...args) { return this.link(...args) },
-  link: (href, title, text) => `${chalk.blue(text)} (${chalk.magenta(href)})`,
-  strong: text => chalk.bold(text),
+  em: text => chalk.green.italic(text),
+  image: function(...args) { return this.link(...args); },
+  link: (href, title, text) => `${chalk.cyan(text)} (${chalk.blue(href)})`,
+  strong: text => chalk.green.bold(text),
   text: text => text
 };
